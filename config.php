5604447288fa4a04a0e10c04a9e334e33372e938
Tymur Hlushan <timur@Tymurs-MacBook-Pro.local>
2015-12-02 13:41:52 +0200

    external config

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
-define("CLIENT_HOST_URL", "http://52.18.45.203/deploy/deploy_1_1");\
+/*
+define("CLIENT_HOST_URL", "http://52.18.45.203/deploy/deploy_1_1");
 define("CLIENT_HOST_PATH_HOME", "/index.php");
 define("SERVER_HOST_URL", "http://52.18.45.203/deploy/deploy_1_2");
 define("SERVER_HOST_PATH_HOME", "/index.php");
@@ -8,8 +9,8 @@ define("DB_HOST", "127.0.0.1");
 define("DB_NAME", "deployment_1_2");
 define("DB_USER", "root");
 define("DB_PASSWORD", "test");
+*/
 
-/*
 define("CLIENT_HOST_URL", "http://52.28.111.94");
 define("CLIENT_HOST_PATH_HOME", "/index.php");
 define("SERVER_HOST_URL", "http://52.29.207.57");
@@ -18,4 +19,3 @@ define("DB_HOST", "deploy-test.ctatdh27he5y.eu-central-1.rds.amazonaws.com");
 define("DB_NAME", "deployment_1_2");
 define("DB_USER", "deploy");
 define("DB_PASSWORD", "HUquNhK0WnrB07AHzb6");
-*/
